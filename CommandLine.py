0b073fa38a369b4d2f7e706ae865200660cb6f1b
Thu Rein Oo <105615488+thurein2003@users.noreply.github.com>
2024-02-25 01:45:58 -0800

    Update CommandLine.py

--- CommandLine.py
+++ CommandLine.py
@@ -5,6 +5,24 @@ from ip2geotools.databases.noncommercial import DbIpCity
 from geopy.distance import distance
 from colorama import Fore 
 import pyfiglet
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init()
+
+# Spider ASCII art
+spider = """
+    ╭━━━━╮
+   ╭┃⛧⛧┃╮╭┳━━━┳╮
+   ┃┃⛧⛧┃┃┃╭━╮┣╋╮┃
+   ╰┫⛧⛧┃╯╰┫╭╯┃┃╰╯
+   ╱┃⛧⛧┃╭╮┃┃╰┫┃╭╮
+   ╱╰━━╯╰╯╰┻━━┻╯╰╯
+"""
+
+
+print(f"{Fore.YELLOW}{spider}")
+print(f"{Fore.GREEN}[*] {Style.RESET_ALL}Running your Python...
 
 def printDis(ip):
     ans = DbIpCity.get(ip, api_key="free")
